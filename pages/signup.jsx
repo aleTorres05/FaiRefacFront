4c46e607ba7a73eaadd4683f0365826de7a3253a
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-09-19 20:54:22 -0600

    [FEAT] create signup page and user registration form component

---
+++ pages/signup.jsx
@@ -0,0 +1,11 @@
+import UserRegistrationForm from '../components/UserRegistrationForm';
+import Header from '@/components/Header';
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <Header/>
+      <UserRegistrationForm />
+    </div>
+  );
+}
