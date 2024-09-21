33f9047b6bca3e7597b121f22577f33d44cbe174
Alejandro Torres <torresalexatlanta@gmail.com>
2024-09-21 08:38:56 -0600

    added client additial info page

---
+++ components/ClientEditInfoForm.jsx
@@ -0,0 +1,5 @@
+export default function ClientEditInfoForm() {
+  const { id } = router.query;
+
+  return <></>;
+}
---
+++ pages/clients/[id]/clientInfo.jsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function clientInfo() {
+  return (
+    <>
+      <main className="my-2">
+        <Header />
+      </main>
+    </>
+  );
+}
