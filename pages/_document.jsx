032bfb361eee940aa1df827e0027380a5c39c6df
gurdiny <gerardour619@gmail.com>
2024-10-04 16:44:25 -0600

    Create/ create thanks Page and confirmation Page

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
