318c5474f24e3eafd8904ae2c4573ca2d1fadb79
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-10-04 10:39:43 -0600

    [FEAT]Add update info page

---
+++ pages/update-info.jsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header';
+import UserUpdateForm from '@/components/UserUpdateForm';
+
+
+export default function UpdateInfo() {
+    return (
+        <div>
+            <Header/>
+            <UserUpdateForm/>
+        </div>
+    )
+}
