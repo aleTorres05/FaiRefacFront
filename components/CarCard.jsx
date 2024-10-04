cd2ae601db41e29161c4c0d31f786588068672fb
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-10-04 13:39:46 -0600

    [FEAT] Add car detail modal, form validation, and router push on forms

--- components/CarCard.jsx
+++ components/CarCard.jsx
@@ -1,48 +1,79 @@
+import { useState, useEffect } from "react";
+import CarDetail from "./CarDetail";
+
 export default function CarCard() {
   const carDetails = [
     {
-      image:
-        "https://acnews.blob.core.windows.net/imgnews/large/NAZ_68778115360b49c0858a0f0bde1e74bb.jpg",
+      image: "https://acnews.blob.core.windows.net/imgnews/large/NAZ_68778115360b49c0858a0f0bde1e74bb.jpg",
       name: "Audi A1 2022 Sport",
+      model: "A1",
+      brand: "Audi",
+      year: "2022",
+      version: "Sport",
     },
     {
       image:
         "https://www.excelenciasdelmotor.com/sites/default/files/u7/Seat-Leon-729x486-c44abb6e2a3f69a9.jpg",
       name: "Seat Ibiza Sport 2008",
+      model: "Ibiza",
+      brand: "SEAT",
+      year: "2008",
+      version: "Sport"
     },
     {
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Tesla_Model_3_%282023%29_Autofr%C3%BChling_Ulm_IMG_9282.jpg/640px-Tesla_Model_3_%282023%29_Autofr%C3%BChling_Ulm_IMG_9282.jpg",
       name: "Tesla Model S 2020",
+      model: "Model S",
+      brand: "Tesla",
+      year: "2022",
+      version: ""
     },
     {
       image:
         "https://autodealerluna.mx/wp-content/uploads/2023/08/WhatsApp-Image-2023-08-03-at-3.27.12-PM.jpeg",
       name: "Chevorlet Cheyenne 2018",
+      model: "Cheyenne",
+      brand: "Chevrolet",
+      year: "2018",
+      version: ""
     },
   ];
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCar, setSelectedCar] = useState(null);
+
+  const openModal = (car) => {
+    setSelectedCar(car);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedCar(null);
+  };
 
   return (
     <>
       <div className="flex flex-col ">
-        {carDetails.map((car, idx) => {
-          return (
-            <div
-              key={`car-${idx}`}
-              className="relative w-full h-[220px] mb-3 rounded-lg lg:p-2 lg:h-[100%] xl:h-[100%] 2xl:h-[100%]"
-              id={`car-${idx}`}
-            >
-              <img
-                className=" opacity-50 rounded-lg h-full w-full"
-                src={car.image}
-                alt="car"
-              />
-              <p className=" font-chakra absolute bottom-3 border-t-2 border-[#D16527] left-3 lg:left-4 lg:bottom-4 xl:h-[10%] xl:w-fit xl:text-2xl xl:font-semibold xl:border-t-4 xl:left-7">
-                {car.name}
-              </p>
-            </div>
-          );
-        })}
+        {carDetails.map((car, idx) => (
+          <div
+            key={`car-${idx}`}
+            className="relative w-full h-[220px] mb-3 rounded-lg lg:p-2 lg:h-[100%] xl:h-[100%] 2xl:h-[100%]"
+            id={`car-${idx}`}
+            onClick={() => openModal(car)}
+          >
+            <img className="opacity-50 rounded-lg h-full w-full" src={car.image} alt="car" />
+            <p className="font-chakra absolute bottom-3 border-t-2 border-[#D16527] left-3 lg:left-4 lg:bottom-4 xl:h-[10%] xl:w-fit xl:text-2xl xl:font-semibold xl:border-t-4 xl:left-7">
+              {car.name}
+            </p>
+          </div>
+        ))}
+
+        {/* Modal */}
+        {isModalOpen && (
+          <CarDetail selectedCar={selectedCar} closeModal={closeModal} />
+        )}
       </div>
     </>
   );
---
+++ components/CarDetail.jsx
@@ -0,0 +1,63 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+
+export default function CarDetail({ selectedCar, closeModal, isModalOpen }) {
+
+    const router = useRouter();
+
+    const handleQuoteClick = () => {
+        router.push('/quote-form')
+    }
+
+    useEffect(() => {
+        if (isModalOpen) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = ""; 
+        }
+
+        return () => {
+          document.body.style.overflow = "";
+        };
+      }, [isModalOpen]);
+
+    return (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+        <div className="relative mt-51 md:mt-4 bg-[#161616] p-8 w-full md:w-3/4 rounded-lg shadow-lg max-h-[80vh] overflow-y-auto">
+          <img
+            src={"https://fairefac-assets.s3.us-east-2.amazonaws.com/Close-orange.png"}
+            alt="Cerrar"
+            className="absolute top-4 right-4 cursor-pointer w-6 h-6"
+            onClick={closeModal}
+          />
+          <h2 className="font-chakra font-bold text-center text-[24px] mb-4">Información del Carro</h2>
+          {/* Mostrar información del carro seleccionado */}
+          {selectedCar && (
+            <div>
+              <div className="max-w-[520px] mx-auto mb-4">
+                <img src={selectedCar.image} alt={selectedCar.name} className="w-full h-auto md:mb-4" />
+              </div>
+              <div className="max-w-[320px] mx-auto">
+                 <div className="flex flex-col md:flex-row justify-between md:mb-4">
+                   <p><strong>Marca:</strong> {selectedCar.brand}</p>
+                   <p><strong>Modelo:</strong> {selectedCar.model}</p>
+                 </div>
+                 <div className="flex flex-col md:flex-row justify-between mb-4">
+                   <p><strong>Año:</strong> {selectedCar.year}</p>
+                   <p><strong>Versión:</strong> {selectedCar.version || 'N/A'}</p>
+                 </div>
+              </div>
+              <div className="w-full text-center">
+              <button 
+               className="w-[250px] py-2 bg-[#D16527] text-white font-chakra font-semibold rounded-md"
+               onClick={handleQuoteClick}>
+                COTIZAR
+              </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
