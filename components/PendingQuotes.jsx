4f9c4c46b8594530059a8789ac368ed794b566ce
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-11-01 05:20:33 -0600

    [FEAT]Add pagination to PendingQuotes component

--- components/PendingQuotes.jsx
+++ components/PendingQuotes.jsx
@@ -1,13 +1,23 @@
 import { useState, useEffect, useMemo } from "react";
 import QuoteToReview from "./QuoteToReview";
 import RepairShopQuoteModal from "./RepairShopQuoteModal";
+import clsx from "clsx";
 
 export default function PendingQuotes({ quotes, setQuotes }) {
   const [selectedQuote, setSelectedQuote] = useState(null);
+  const[currentPage, setCurrentPage] = useState(1);
+  const quotesPerPage = 6;
 
   const initialQuotes = useMemo(() => {
     return quotes?.filter((quote) => quote.status === "initial");
   }, [quotes]);
+
+  const totalPages = Math.ceil(initialQuotes.length / quotesPerPage);
+
+  const displayedQuotes = initialQuotes.slice(
+    (currentPage - 1) * quotesPerPage,
+    currentPage * quotesPerPage
+  );
 
   const handleQuoteClick = (quote) => {
     setSelectedQuote(quote);
@@ -23,6 +33,16 @@ export default function PendingQuotes({ quotes, setQuotes }) {
     );
   };
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  }
+
+  useEffect(() => {
+    if( currentPage > totalPages) {
+      setCurrentPage(1);
+    }
+  },[totalPages, currentPage]);
+
   useEffect(() => {
     document.body.style.overflow = selectedQuote ? "hidden" : "";
     return () => {
@@ -36,7 +56,7 @@ export default function PendingQuotes({ quotes, setQuotes }) {
         COTIZACIONES PENDIENTES
       </h2>
       <ul>
-        {initialQuotes?.map((quote) => (
+        {displayedQuotes?.map((quote) => (
           <QuoteToReview
             key={quote._id}
             quote={quote}
@@ -45,6 +65,20 @@ export default function PendingQuotes({ quotes, setQuotes }) {
           />
         ))}
       </ul>
+      <div className="flex justify-center mt-4">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            onClick={() => handlePageChange(index + 1)}
+            className={clsx(
+              "border font-chakra border-[#D26528] mr-2 h-[100%] w-[9%] md:w-[10%] lg:w-[8%] xl:w-[5%] hover:bg-[#D26528]",
+              currentPage === index + 1 ? "bg-[#D26528]" : "bg-black"
+            )}
+            key={index}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
       {selectedQuote && (
         <RepairShopQuoteModal
           selectedQuote={selectedQuote}
