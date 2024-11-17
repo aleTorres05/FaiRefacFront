440ed15779834298f58a2c87764c978ba09c9ef2
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-11-16 20:24:00 -0600

    [CHORE]Move Header to dashboard page

--- components/RepairShopDashboard.jsx
+++ components/RepairShopDashboard.jsx
@@ -7,12 +7,11 @@ import { createAccountLink, activateStripeAccount } from "@/pages/api/user";
 import { toast } from "sonner";
 import { useRouter } from "next/router";
 
-
 export default function RepairShopDashboard({ user }) {
   const [isSelected, setIsSelected] = useState({
-    pendingQuotes: true, 
+    pendingQuotes: true,
     followUpPanel: false,
-    updatePaymentInfo: false, 
+    updatePaymentInfo: false,
   });
 
   const router = useRouter();
@@ -37,7 +36,7 @@ export default function RepairShopDashboard({ user }) {
       toast.error("Es necesario iniciar sesión para acceder a esta página.");
       router.push("/login");
       return;
-    } 
+    }
     try {
       const response = await createAccountLink(repairShop._id, token);
       toast.info("Redireccionando a Stripe...");
@@ -45,7 +44,6 @@ export default function RepairShopDashboard({ user }) {
     } catch (error) {
       toast.error(error.message || "Ocurrió un error inesperado");
     }
-
   };
 
   useEffect(() => {
@@ -62,7 +60,10 @@ export default function RepairShopDashboard({ user }) {
         toast.success("Cuenta Stripe activada exitosamente.");
         window.location.reload();
       } catch (error) {
-        toast.error(error.message || "Ocurrió un error al actualizar la información de pago.");
+        toast.error(
+          error.message ||
+            "Ocurrió un error al actualizar la información de pago."
+        );
       }
     };
 
@@ -70,45 +71,51 @@ export default function RepairShopDashboard({ user }) {
       updateStripeStatus();
     }
   }, [repairShop, router]);
-  
+
   if (!repairShop?.stripeAccountActive) {
     return (
       <main className="flex flex-col items-center text-center justify-center h-screen bg-black text-white">
         <h2 className="text-2xl font-chakra uppercase font-bold mb-4 p-8">
-          Dirigete a nuestra plataforma de pago y agrega tu información para recibir pagos
+          Dirigete a nuestra plataforma de pago y agrega tu información para
+          recibir pagos
         </h2>
-        <button 
+        <button
           onClick={handleUpdatePaymentInfo}
           className="bg-[#D16527] px-6 py-2 font-chakra uppercase rounded-md"
         >
-          Actualizar 
+          Actualizar
         </button>
       </main>
     );
   }
 
   return (
-    <>
-      <main className="md:mt-[18px] md:mx-[32px] flex flex-col md:gap-4">
-        <Header user={user}/>
-        <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-12 gap-0 p-3 md:p-0">
-          <DashboardButtons isSelected={isSelected} handleButtonSelection={handleButtonSelection} repairShop={repairShop}/>
+    <main className="md:mt-[18px] md:mx-[32px] flex flex-col md:gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-12 gap-0 p-3 md:p-0">
+        <DashboardButtons
+          isSelected={isSelected}
+          handleButtonSelection={handleButtonSelection}
+          repairShop={repairShop}
+        />
 
-
-          <div className="bg-black h-fit col-start-1 col-end-13 md:p-2 p-4 lg:col-start-4 lg:col-end-13 md:col-start-3 md:col-end-13 md:ml-4 rounded-2xl mt-2 md:h-screen md:flex-col lg:mt-4 lg:p-8 xl:col-start-4 xl:col-end-13 2xl:col-start-4 2xl:col-end-13 xl:p-7">
-            {isSelected.pendingQuotes && <PendingQuotes quotes={quotes} setQuotes={setQuotes}/>}
-            {isSelected.followUpPanel && <FollowUpPanel repairShop={repairShop}/>}
-            {isSelected.updatePaymentInfo && 
+        <div className="bg-black h-fit col-start-1 col-end-13 md:p-2 p-4 lg:col-start-4 lg:col-end-13 md:col-start-3 md:col-end-13 md:ml-4 rounded-2xl mt-2 md:h-screen md:flex-col lg:mt-4 lg:p-8 xl:col-start-4 xl:col-end-13 2xl:col-start-4 2xl:col-end-13 xl:p-7">
+          {isSelected.pendingQuotes && (
+            <PendingQuotes quotes={quotes} setQuotes={setQuotes} />
+          )}
+          {isSelected.followUpPanel && (
+            <FollowUpPanel repairShop={repairShop} />
+          )}
+          {isSelected.updatePaymentInfo && (
             <main className="flex flex-col items-center  md:justify-center h-screen bg-black text-white">
-            <h2 className="text-base md:text-2xl font-chakra uppercase font-bold mb-4 p-8">
-            En Stripe podrás actualizar tu información. Recuerda que es importante mantener tus datos actualizados, ya que recibirás tus pagos en esta cuenta.
-            </h2>
-          </main>
-            }
-          </div>
+              <h2 className="text-base md:text-2xl font-chakra uppercase font-bold mb-4 p-8">
+                En Stripe podrás actualizar tu información. Recuerda que es
+                importante mantener tus datos actualizados, ya que recibirás tus
+                pagos en esta cuenta.
+              </h2>
+            </main>
+          )}
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
-
