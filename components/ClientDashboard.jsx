31a2b684f86387a1202e8833208e247460baecf3
gurdiny <gerardour619@gmail.com>
2024-11-18 22:10:58 -0600

    Style and popups

--- components/ClientDashboard.jsx
+++ components/ClientDashboard.jsx
@@ -28,53 +28,53 @@ export default function ClientDashboard({ user, clientCarWithQuotes }) {
   }, [user, clientCarWithQuotes]);
 
   return (
-      <main className=" mt-[18px] mx-[32px] grid lg:grid-cols-12 md:grid-cols-12  ">
-        <div className="bg-black col-start-1 col-end-12 h-fit md:p-2 lg:col-start-1 lg:col-end-5 md:col-start-1 md:col-end-5 p-2 rounded-2xl mt-2 md:h-screen lg:h-full  md:flex-col lg:mt-4 lg:p-3 xl:col-start-1 xl:col-end-4 2xl:col-start-1 2xl:col-end-4 ">
-          <div>
-            <div className="flex flex-row mb-1 md:flex-row lg:mb-10 justify-start lg:mr-3 ">
-              <img
-                className="mr-3 lg:w-[80px] lg:h-[80px] h-[20%] w-[20%] md:h-[40%] md:w-[40%] rounded-full"
-                src={user.client?.profilePicture}
-                alt="avatar"
-              />
-              <div className="flex flex-col flex-wrap md:justify-center md:flex-col justify-center xl:w-full  2xl:w-full xl:p-2 2xl:p-2">
-                <h4 className="font-chakra font-bold lg:h-[24px] md:h-[20px] md:text-[12px] lg:text-[17px] xl 2xl:text-[25px] md:w-[100%] md:mb-2 uppercase  xl:w-[full] 2xl:w-[full]">
-                  {`${user.client?.firstName} ${user.client?.lastName}`}
-                </h4>
-              </div>
+    <div className=" mt-[18px] mx-[32px] grid lg:grid-cols-12 md:grid-cols-12  ">
+      <div className="bg-black col-start-1 col-end-12 h-fit md:p-2 lg:col-start-1 lg:col-end-5 md:col-start-1 md:col-end-5 p-2 rounded-2xl mt-2 md:h-screen lg:h-full  md:flex-col lg:mt-4 lg:p-3 xl:col-start-1 xl:col-end-4 2xl:col-start-1 2xl:col-end-4 ">
+        <div>
+          <div className="flex flex-row mb-1 md:flex-row lg:mb-10 justify-start lg:mr-3 ">
+            <img
+              className="mr-3 lg:w-[80px] lg:h-[80px] h-[20%] w-[20%] md:h-[40%] md:w-[40%] rounded-full"
+              src={user.client?.profilePicture}
+              alt="avatar"
+            />
+            <div className="flex flex-col flex-wrap md:justify-center md:flex-col justify-center xl:w-full  2xl:w-full xl:p-2 2xl:p-2">
+              <h4 className="font-chakra font-bold lg:h-[24px] md:h-[20px] md:text-[12px] lg:text-[17px] xl 2xl:text-[25px] md:w-[100%] md:mb-2 uppercase  xl:w-[full] 2xl:w-[full]">
+                {`${user.client?.firstName} ${user.client?.lastName}`}
+              </h4>
             </div>
           </div>
-          <div className="flex flex-row gap-9 md:gap-6 md:flex-col justify-center md:mt-4 md:items-center  mb-2.5">
-            <button
-              onClick={() => handleButtonSelection("coches")}
-              className={clsx(
-                "lg:w-[90%] font-chakra xl:text-[25px]  2xl:text-[25px] font-semibold lg:h-[50px] md:w-[80%] w-[35%] h-[30px]  md:h-[40px]  xl:w-[90%] 2xl:w-[90%]",
-                isSelected.coches
-                  ? "bg-[#D26528] text-white"
-                  : "bg-white text-black"
-              )}
-            >
-              Coches
-            </button>
-            <button
-              onClick={() => handleButtonSelection("cotizaciones")}
-              className={clsx(
-                "lg:w-[90%] font-chakra text-[12px] xl:text-[25px] 2xl:text-[25px] font-semibold lg:h-[50px] md:w-[80%] w-[35%] h-[30px]  md:h-[40px]  xl:w-[90%] 2xl:w-[90%]",
-                isSelected.cotizaciones
-                  ? "bg-[#D26528] text-white"
-                  : "bg-white text-black"
-              )}
-            >
-              Cotizaciones
-            </button>
-          </div>
         </div>
-        <div className="bg-black h-fit col-start-1 col-end-13 md:p-2 p-3.5 lg:col-start-5 lg:col-end-13 md:col-start-5 md:col-end-13 md:ml-4 rounded-2xl mt-2 md:h-fit lg:h-fit  md:flex-col  lg:mt-4 lg:p-3  xl:col-start-4 xl:col-end-13 2xl:col-start-4 2xl:col-end-13 xl:p-7">
-          {isSelected.coches && <ClientCar />}
-          {isSelected.cotizaciones && (
-            <ClientQuotes carListQuotes={clientCarWithQuotes} />
-          )}
+        <div className="flex flex-row gap-9 md:gap-6 md:flex-col justify-center md:mt-4 md:items-center  mb-2.5">
+          <button
+            onClick={() => handleButtonSelection("coches")}
+            className={clsx(
+              "lg:w-[90%] font-chakra xl:text-[25px]  2xl:text-[25px] font-semibold lg:h-[50px] md:w-[80%] w-[35%] h-[30px]  md:h-[40px]  xl:w-[90%] 2xl:w-[90%]",
+              isSelected.coches
+                ? "bg-[#D26528] text-white"
+                : "bg-white text-black"
+            )}
+          >
+            Coches
+          </button>
+          <button
+            onClick={() => handleButtonSelection("cotizaciones")}
+            className={clsx(
+              "lg:w-[90%] font-chakra text-[12px] xl:text-[25px] 2xl:text-[25px] font-semibold lg:h-[50px] md:w-[80%] w-[35%] h-[30px]  md:h-[40px]  xl:w-[90%] 2xl:w-[90%]",
+              isSelected.cotizaciones
+                ? "bg-[#D26528] text-white"
+                : "bg-white text-black"
+            )}
+          >
+            Cotizaciones
+          </button>
         </div>
-      </main>
+      </div>
+      <div className="bg-black h-fit col-start-1 col-end-13 md:p-2 p-3.5 lg:col-start-5 lg:col-end-13 md:col-start-5 md:col-end-13 md:ml-4 rounded-2xl mt-2 md:h-fit lg:h-fit  md:flex-col  lg:mt-4 lg:p-3  xl:col-start-4 xl:col-end-13 2xl:col-start-4 2xl:col-end-13 xl:p-7">
+        {isSelected.coches && <ClientCar />}
+        {isSelected.cotizaciones && (
+          <ClientQuotes carListQuotes={clientCarWithQuotes} />
+        )}
+      </div>
+    </div>
   );
 }
--- pages/quote-form.jsx
+++ pages/quote-form.jsx
@@ -1,308 +1,173 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { toast } from "sonner";
-import MechanicForm from "@/components/MechanicForm";
-import { getAllMechanics } from "./api/mechanic";
-import { useForm, useFieldArray } from "react-hook-form";
-import { createQuote, createQuoteLinkToken, validateToken } from "./api/quote";
+const [timeLeft, setTimeLeft] = useState(0);
+const { token } = router.query;
 
-export default function QuoteForm() {
-  const router = useRouter();
+const [isInfoVisible, setIsInfoVisible] = useState(false);
+const { handleSubmit, register } = useForm();
 
-  const [isMechanicFormOpen, setIsMechanicFormOpen] = useState(false);
-  const [mechanics, setMechanics] = useState([]);
-  const [quoteToken, setQuoteToken] = useState({});
-  const [selectedMechanic, setSelectedMechanic] = useState("");
-  const handleOpenMechanicForm = () => setIsMechanicFormOpen(true);
-  const handleCloseMechanicForm = () => setIsMechanicFormOpen(false);
-  const [car, setCar] = useState(null);
-  const [client, setClient] = useState(null);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const { token } = router.query;
-
-  const {
-    handleSubmit,
-    register,
-    control,
-    formState: { errors },
-    setError,
-  } = useForm({ defaultValues: { items: [{ concept: "", quantity: 1 }] } });
-
-  const { fields, append, remove, update } = useFieldArray({
-    control,
-    name: "items",
-  });
-
-  const parseJwt = (token) => {
-    try {
-      const base64Url = token.split(".")[1];
-      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-      const jsonPayload = decodeURIComponent(
-        atob(base64)
-          .split("")
-          .map((c) => `%${("00" + c.charCodeAt(0).toString(16)).slice(-2)}`)
-          .join("")
-      );
-      return JSON.parse(jsonPayload);
-    } catch (error) {
-      console.error("Error decoding token:", error);
-      return null;
-    }
-  };
-
-  const getTokenExpirationTime = (token) => {
-    try {
-      const decoded = parseJwt(token);
-      const currentTime = Math.floor(Date.now() / 1000);
-      return decoded.exp - currentTime; // Time left in seconds
-    } catch (error) {
-      console.error("Error decoding token:", error);
-      return 0; // Return 0 if there's an error, indicating expiration
-    }
-  };
-
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${minutes}:${secs < 10 ? `0${secs}` : secs}`; // e.g., "5:09"
-  };
-
-  const isTokenExpired = (token) => {
-    try {
-      if (token) {
-        const decoded = parseJwt(token);
-        const currentTime = Math.floor(Date.now() / 1000);
-        return decoded.exp < currentTime;
-      }
-    } catch (error) {
-      console.error("Error decoding token:", error);
-      return true;
-    }
-  };
-
-  useEffect(() => {
-    const carSelected = sessionStorage.getItem("carSelected");
-    const clientSelected = sessionStorage.getItem("client");
-
-    if (carSelected && clientSelected) {
-      const clientCar = JSON.parse(carSelected);
-      const client = JSON.parse(clientSelected);
-      setCar(clientCar._id);
-      setClient(client._id);
-    } else if (token && !isTokenExpired(token)) {
-      // Update time left every second
-      const intervalId = setInterval(() => {
-        const newTimeLeft = getTokenExpirationTime(token);
-        setTimeLeft(newTimeLeft);
-
-        if (newTimeLeft <= 0) {
-          clearInterval(intervalId);
-          router.push("/");
-          toast.error("Session Expirada, Solicite Nuevo Enlace");
-        }
-      }, 1000);
-
-      return () => clearInterval(intervalId);
-    } else if (!token && isTokenExpired(token)) {
-      const intervalId = setInterval(() => {
-        if (isTokenExpired(token)) {
-          clearInterval(intervalId);
-          router.push("/timeOutPage");
-          toast.error("Session Expirada, Solicite Nuevo Enlace2");
-        }
-      }, 60000);
-
-      return () => clearInterval(intervalId);
-    }
-
-    getAllMechanics()
-      .then((mechanics) => {
-        setMechanics(mechanics);
-      })
-      .catch((e) => {
-        toast.error("Algo salio mal: ", e);
-        throw new Error(e);
-      });
-  }, [token, router.isReady]);
-
-  const handleMechanicChange = (e) => {
-    setSelectedMechanic(e.target.value);
-  };
-
-  async function submitForm(data) {
-    try {
-      if (!selectedMechanic) {
-        toast.warning("Selecciona un Mecanico");
-        return;
-      }
-      if (token) {
-        const tokenValidated = await validateToken(token, router);
-        setCar(tokenValidated.carId);
-      }
-
-      const response = await createQuote(car, selectedMechanic, data);
-      if (response.success) {
-        router.push("/quote-sent");
-      }
-    } catch (error) {}
+const getTokenExpirationTime = (token) => {
+  try {
+    const decoded = parseJwt(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decoded.exp - currentTime;
+  } catch (error) {
+    console.error("Error decoding token:", error);
+    return 0;
   }
+};
 
-  async function handleCopyUrl() {
-    const currentUrl = window.location.href;
-    if (quoteToken) {
-      setQuoteToken({});
-    }
-    console.log(client, car);
-    const token = await createQuoteLinkToken(client, car);
-    setQuoteToken({ token });
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${minutes}:${secs < 10 ? `0${secs}` : secs}`;
+};
 
-    navigator.clipboard
-      .writeText(`${currentUrl}?token=${token}`)
-      .then(() => {
-        toast.success("URL copiada al portapapeles");
-      })
-      .catch((err) => {
-        toast.error("Error al copiar la URL: ", err);
-      });
+const isTokenExpired = (token) => {
+  const expirationTime = getTokenExpirationTime(token);
+  return expirationTime <= 0;
+};
+
+useEffect(() => {
+  if (token && !isTokenExpired(token)) {
+    const intervalId = setInterval(() => {
+      const newTimeLeft = getTokenExpirationTime(token);
+      setTimeLeft(newTimeLeft);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }
+}, [token]);
 
-  return (
-    <div className="flex flex-col w-full p-4 justify-center md:items-center min-h-screen">
-      <h1 className="font-chakra mb-14 md:mb-[80px] w-full text-center text-[32px] font-bold">
-        SOLICITA UNA COTIZACIÓN
-      </h1>
-      <div className="flex flex-row w-full mb-14 md:max-w-[692px] justify-end items-center">
-        {!token ? (
-          <div className="w-full flex flex-row items-center">
+return (
+  <div className="flex flex-col w-full px-4 md:px-10 justify-center md:items-center min-h-screen">
+    <h1 className="font-chakra mb-10 md:mb-16 w-full text-center text-[24px] md:text-[32px] font-bold">
+      SOLICITA UNA COTIZACIÓN
+    </h1>
+    <div className="flex flex-col md:flex-row w-full mb-14 md:max-w-[692px] justify-between items-center">
+      {!token ? (
+        <div
+          className="relative"
+          onMouseEnter={() => setIsInfoVisible(true)}
+          onMouseLeave={() => setIsInfoVisible(false)}
+        >
+          <button className="flex flex-row items-center">
             <img
-              className="h-[50px]"
+              className="h-[40px] md:h-[50px]"
               src="https://fairefac-assets.s3.us-east-2.amazonaws.com/Copy.png"
               alt="copy-icon"
               onClick={handleCopyUrl}
             />
-            <p className="font-chakra">Copiar Enlace</p>
-          </div>
-        ) : (
-          <div className="w-full flex flex-row items-center">
-            <p className=""> Session expira en: {formatTime(timeLeft)}</p>
-          </div>
-        )}
-        <div className="flex flex-col mr-4 sm:mr-9 min-w-[150px] md:min-w-[250px] items-center">
-          <label
-            htmlFor="mechanic-select"
-            className="block text-white w-full font-chakra font-bold"
-          >
-            SELECCIONA UN TALLER
-          </label>
-          <select
-            id="mechanic-select"
-            value={selectedMechanic}
-            onChange={handleMechanicChange}
-            className="text-[#C2C2C2] w-full outline-none font-mulish text-[14px] font-normal leading-normal bg-transparent pb-3 border-b border-b-[#343434] border-b-1"
-          >
-            <option className="text-white font-mulish" value="" disabled>
-              Selecciona un taller
-            </option>
-            {mechanics.map((mechanic) => (
-              <option key={mechanic._id} value={mechanic._id}>
-                {mechanic.workshopName}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex h-full justify-center my-auto">
-          <button
-            className="bg-[#D16527] text-white font-chakra min-w-[50px] max-h-10 p-2 rounded-md"
-            type="button"
-            onClick={handleOpenMechanicForm}
-          >
-            +
+            <p className="font-chakra ml-2">Copiar Link</p>
           </button>
+          {isInfoVisible && (
+            <div className="absolute top-[-60px] left-1/2 transform -translate-x-1/2 bg-black text-white p-2 rounded-md shadow-lg text-sm w-[200px] text-center z-50">
+              ¿No estás seguro de qué refacciones necesitas? Haz clic para
+              compartir con tu mecánico.
+            </div>
+          )}
         </div>
+      ) : (
+        <div className="flex items-center justify-center w-full md:justify-end">
+          <p className="font-mulish text-[12px] md:text-[14px]">
+            Sesión expira en: {formatTime(timeLeft)}
+          </p>
+        </div>
+      )}
+      <div className="flex flex-col w-full mt-4 md:mt-0 sm:w-1/2">
+        <label
+          htmlFor="mechanic-select"
+          className="block text-white w-full font-chakra font-bold"
+        >
+          Selecciona un taller
+        </label>
+        <select
+          id="mechanic-select"
+          value={selectedMechanic}
+          onChange={handleMechanicChange}
+          className="text-[#C2C2C2] w-full outline-none font-mulish text-[14px] font-normal leading-normal bg-transparent pb-3 border-b border-b-[#343434]"
+        >
+          <option className="text-white font-mulish" value="" disabled>
+            Selecciona un taller
+          </option>
+          {mechanics.map((mechanic) => (
+            <option key={mechanic.id} value={mechanic.id}>
+              {mechanic.name}
+            </option>
+          ))}
+        </select>
       </div>
+      <div className="flex h-full justify-center mt-4 md:mt-0">
+        <button
+          className="bg-[#D16527] text-white font-chakra min-w-[50px] max-h-10 p-2"
+          type="button"
+          onClick={handleOpenMechanicForm}
+        >
+          +
+        </button>
+      </div>
+    </div>
 
-      <MechanicForm
-        isOpen={isMechanicFormOpen}
-        onClose={handleCloseMechanicForm}
-      />
-      <form
-        onSubmit={handleSubmit(submitForm)}
-        className="flex flex-col gap-4 my-4"
-      >
-        {fields.map((field, index) => (
-          <div key={index} className="flex items-center gap-4">
+    <MechanicForm
+      className="popup-container"
+      isOpen={isMechanicFormOpen}
+      onClose={handleCloseMechanicForm}
+    />
+
+    <form
+      onSubmit={handleSubmit(submitForm)}
+      className="flex flex-col gap-4 my-4 w-full max-w-lg md:max-w-3xl mx-auto"
+    >
+      {fields.map((field, index) => (
+        <div
+          key={index}
+          className="flex flex-col md:flex-row items-center gap-4"
+        >
+          <input
+            type="text"
+            placeholder="Refacción"
+            className="text-white text-base outline-none font-mulish text-[14px] font-normal leading-normal w-full md:w-3/4 bg-transparent pb-3 border-b border-b-[#343434]"
+            {...register(`items.${index}.concept`, {
+              required: { value: true, message: "Refacción es requerida" },
+            })}
+          />
+          <div className="flex items-center">
             <input
               type="text"
-              placeholder="Refacción"
-              className="text-white text-base outline-none font-mulish text-[14px] font-normal leading-normal md:min-w-[600px] md:max-w-[850px] w-full bg-transparent pb-3 border-b border-b-[#343434] border-b-1"
-              {...register(`items.${index}.concept`, {
-                required: { value: true, message: "Refaccion es Requerida" },
+              value={field.quantity}
+              readOnly
+              className="font-chakra px-2 bg-transparent text-center w-10"
+              {...register(`items.${index}.quantity`, {
+                valueAsNumber: true,
               })}
             />
-            <div className="flex items-center">
-              <button
-                type="button"
-                onClick={() => {
-                  update(index, {
-                    ...field,
-                    quantity: Math.max(1, field.quantity - 1),
-                  });
-                }}
-                className="font-chakra p-2 border"
-              >
-                -
-              </button>
-              <input
-                type="text"
-                value={field.quantity}
-                readOnly
-                className="font-chakra px-2 bg-transparent text-center w-10 "
-                {...register(`items.${index}.quantity`, {
-                  valueAsNumber: true,
-                })}
-              />
-
-              <button
-                type="button"
-                onClick={() =>
-                  update(index, { ...field, quantity: field.quantity + 1 })
-                }
-                className="font-chakra p-2 border"
-              >
-                +
-              </button>
-              <button
-                onClick={() => {
-                  remove(index);
-                }}
-                className="font-chakra p-2 border-2 border-[#D16527] ml-4"
-                type="button"
-              >
-                Eliminar
-              </button>
-            </div>
+            <button
+              type="button"
+              onClick={() => incrementQuantity(index)}
+              className="bg-[#D16527] text-white font-chakra min-w-[50px] max-h-10 p-2"
+            >
+              +
+            </button>
           </div>
-        ))}
-
-        <div className="w-full flex justify-center">
-          <button
-            type="button"
-            onClick={() => append({ concept: "", quantity: 1 })}
-            className="bg-[#D16527] md:mt-6 md:mb-14 w-[250px] font-chakra font-bold text-white p-2 rounded-md"
-          >
-            AGREGAR PIEZA
-          </button>
         </div>
+      ))}
 
-        <div className="w-full flex justify-center md:justify-end">
-          <button
-            type="submit"
-            className="bg-[#D16527] w-[250px] font-chakra font-bold text-white p-2 rounded-md mt-4"
-          >
-            SOLICITAR COTIZACIÓN
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
+      <div className="w-full flex justify-center mt-4">
+        <button
+          type="button"
+          onClick={() => append({ concept: "", quantity: 1 })}
+          className="bg-[#D16527] w-[200px] md:w-[250px] font-chakra font-bold text-white p-2"
+        >
+          AGREGAR PIEZA
+        </button>
+      </div>
+
+      <div className="w-full flex justify-center md:justify-end mt-4">
+        <button
+          type="submit"
+          className="bg-[#D16527] w-[200px] md:w-[250px] font-chakra font-bold text-white p-2"
+        >
+          SOLICITAR COTIZACIÓN
+        </button>
+      </div>
+    </form>
+  </div>
+);
